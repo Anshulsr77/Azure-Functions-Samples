d9dcd25dc22d5f3bd879c3aff2939a7bb689166a
Anshul Srivastava <asrivastava@hso.com>
2021-08-09 13:56:56 +0530

    Added JSON Restructuring function

---
+++ JSONRestructuringFunc/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JSONRestructuringFunc
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public string Company { get; set; }
+        public int Age { get; set; }
+        // public string Country { get; set; }
+    }
+
+    public class Employees
+    {
+        public static List<Employee> employees { get; set; } = new List<Employee>();
+    }
+
+}
---
+++ JSONRestructuringFunc/JSONreformat.cs
@@ -0,0 +1,54 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace JSONRestructuringFunc
+{
+    public static class JSONreformat
+    {
+        [FunctionName("JSONreformat")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string name = req.Query["name"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            // name = name ?? data?.name;
+            
+            //Employees emplist = new Employees();
+
+            int l = 0;
+            foreach (var i in data.employees)
+            {
+                Employee emp = new Employee();
+                emp.Name = i.Name;
+                emp.Age = i.Age;
+                emp.Company = i.Company;
+                //emp.Country = data.employees[i].Country;
+                Employees.employees?.Add(emp);
+                l++;
+
+            }
+
+
+
+
+            //string responseMessage = string.IsNullOrEmpty(name)
+            //    ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+            //    : $"Hello, {name}. This HTTP triggered function executed successfully.";
+
+            return new OkObjectResult(Employees.employees);
+        }
+    }
+}
